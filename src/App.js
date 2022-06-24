825214834bbea5f1b4a5491140e455c4f9a26c9b
emilioCode <emilio_mem@hotmail.com>
2022-06-24 12:04:54 -0400

    setting States Parent from Child Component.

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
       <header className="App-header">
         <h1 className="title">Rick & Morty</h1>
         { characters ? (
-          <Characters characters={characters}/>
+          <Characters characters={characters} setCharacters={setCharacters}/>
         ): (
           <>
           <img src={imageRickMorty} alt="Rick & Morty" className="img-home"/>
